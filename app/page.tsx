6aa84e7e6af4b87ddf9bd682d5665bf937ddd672
libranetx <tadiostsegaye7@gmail.com>
2026-02-05 21:19:27 +0300

    create interface for weather data and forcast item

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
 import Image from "next/image";
 
+interface WeatherData {
+  temp: number
+  condition: string
+  humidity: number
+  windSpeed: number
+  visibility: number
+  feelsLike: number
+  pressure: number
+  uvIndex: number
+}
+
+interface ForecastItem {
+  time: string
+  temp: number
+  condition: string
+  precipitation: number
+}
+
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
